8cdafa60fed9b08c54c6ac36b60b25852704fec3
aditya-041 <157344288+aditya-041@users.noreply.github.com>
2024-07-28 17:08:42 +0530

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -11,10 +11,6 @@ const Header = () => {
       <span className="ml-3 text-xl">Stock Management System</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">First Link</a>
-      <a className="mr-5 hover:text-gray-900">Second Link</a>
-      <a className="mr-5 hover:text-gray-900">Third Link</a>
-      <a className="mr-5 hover:text-gray-900">Fourth Link</a>
     </nav>
     
   </div>
